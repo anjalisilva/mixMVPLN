648734e1808d2bf810650d7d011d8c61577478d6
Anjali Silva <anjali.silva@uhnresearch.ca>
2020-03-24 19:26:09 -0400

    Added testing for test-mvplnDataGenerator.R

--- tests/testthat/test-mvplnDataGenerator.R
+++ tests/testthat/test-mvplnDataGenerator.R
@@ -14,11 +14,12 @@ test_that("Data generation is as expected", {
   true_M1 <- matrix(rep(6, (true_r*true_p)), ncol = true_p, nrow = true_r, byrow = TRUE)
   true_M2 <- matrix(rep(1, (true_r*true_p)), ncol = true_p, nrow = true_r, byrow = TRUE)
   true_M_all <- rbind(true_M1, true_M2)
+
   # Phi is a r x r matrix
   library(clusterGeneration)
   # Covariance matrix containing variances and covariances between r occasions
   true_Phi1 <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
-                                                    dim=true_r,
+                                                    dim = true_r,
                                                     rangeVar = c(1, 1.7))$Sigma
   true_Phi1[1, 1] <- 1 # for identifiability issues
   true_Phi2 <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
@@ -26,6 +27,7 @@ test_that("Data generation is as expected", {
                                                     rangeVar = c(0.7, 0.7))$Sigma
   true_Phi2[1, 1] <- 1 # for identifiability issues
   true_Phi_all <- rbind(true_Phi1, true_Phi2)
+
   # Omega is a p x p matrix
   # Covariance matrix containing variance and covariances of p responses/variables
   true_Omega1 <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
@@ -44,11 +46,11 @@ test_that("Data generation is as expected", {
                                         phi = true_Phi_all,
                                         omega = true_Omega_all)
 
-  expect_that(length(simulatedMVdata), equals(9))
-  expect_that(simulatedMVdata, is_a("mvplnDataGenerator"))
-  expect_that(trunc(simulatedMVdata$units), equals(true_n))
-  expect_that(trunc(simulatedMVdata$occassions), equals(true_r))
-  expect_that(trunc(simulatedMVdata$variables), equals(true_p))
+  testthat::expect_that(length(simulatedMVdata), equals(9))
+  testthat::expect_that(simulatedMVdata, is_a("mvplnDataGenerator"))
+  testthat::expect_that(trunc(simulatedMVdata$units), equals(true_n))
+  testthat::expect_that(trunc(simulatedMVdata$occassions), equals(true_r))
+  testthat::expect_that(trunc(simulatedMVdata$variables), equals(true_p))
 })
 
 
@@ -56,7 +58,8 @@ context("Checking for invalid user input")
 test_that("Data generate error upon invalid user input", {
 
   # nOccasions provided as character
-  expect_error(simulatedMVdata <- mvplnDataGenerator(nOccasions = "true_r",
+  testthat::expect_error(simulatedMVdata <- mvplnDataGenerator(
+                                                     nOccasions = "true_r",
                                                      nResponses = true_p,
                                                      nUnits = true_n,
                                                      mixingProportions = c(0.79, 0.21),
@@ -64,8 +67,9 @@ test_that("Data generate error upon invalid user input", {
                                                      phi = true_Phi_all,
                                                      omega = true_Omega_all))
 
-  # nOccasions provided as character
-  expect_error(simulatedMVdata <- mvplnDataGenerator(nOccasions = true_r,
+  # nResponses provided as vector
+  testthat::expect_error(simulatedMVdata <- mvplnDataGenerator(
+                                                     nOccasions = true_r,
                                                      nResponses = c(true_p, true_p),
                                                      nUnits = true_n,
                                                      mixingProportions = c(0.79, 0.21),
@@ -73,49 +77,101 @@ test_that("Data generate error upon invalid user input", {
                                                      phi = true_Phi_all,
                                                      omega = true_Omega_all))
 
-
-  # Generating simulated data - mu has incorrect dimension
-  trueMu1 <- c(6.5, 6, 6, 6, 6)
-  trueMu2 <- c(2, 2.5, 2, 2, 2)
-
-  expect_error(mplnDataGenerator(nObservations = 50,
-    dimensionality = 6,
-    mixingProportions = c(0.79, 0.21),
-    mu = rbind(trueMu1, trueMu2),
-    sigma = rbind(trueSigma1, trueSigma2),
-    produceImage = "No"))
+  # nUnits provided as a decimal
+  testthat::expect_error(simulatedMVdata <- mvplnDataGenerator(
+                                                     nOccasions = true_r,
+                                                     nResponses = true_p,
+                                                     nUnits = 1000.25,
+                                                     mixingProportions = c(0.79, 0.21),
+                                                     matrixMean = true_M_all,
+                                                     phi = true_Phi_all,
+                                                     omega = true_Omega_all))
 
 
-  # Generating simulated data - sigma has incorrect dimension
-  trueMu1 <- c(6.5, 6, 6, 6, 6, 6)
-  trueMu2 <- c(2, 2.5, 2, 2, 2, 2)
-  trueSigma1 <- diag(5) * 2
-  trueSigma2 <- diag(5)
-
-  expect_error(mplnDataGenerator(nObservations = 50,
-    dimensionality = 6,
-    mixingProportions = c(0.79, 0.21),
-    mu = rbind(trueMu1, trueMu2),
-    sigma = rbind(trueSigma1, trueSigma2),
-    produceImage = "No"))
+  # mixingProportions doesn't sum to 1
+  testthat::expect_error(simulatedMVdata <- mvplnDataGenerator(
+                                                    nOccasions = true_r,
+                                                    nResponses = true_p,
+                                                    nUnits = true_n,
+                                                    mixingProportions = c(0.8, 0.21),
+                                                    matrixMean = true_M_all,
+                                                    phi = true_Phi_all,
+                                                    omega = true_Omega_all))
 
 
-  # mixingProportions does not sum to 1
-  expect_error(mplnDataGenerator(nObservations = 50,
-    dimensionality = 6,
-    mixingProportions = c(0.79, 0.2),
-    mu = rbind(trueMu1, trueMu2),
-    sigma = rbind(trueSigma1, trueSigma2),
-    produceImage = "No"))
+  # Generating simulated data - M has incorrect dimension
+  M1_testing <- matrix(rep(6, (true_r*(true_p + 1))),
+                       ncol = (true_p + 1),
+                       nrow = true_r, byrow = TRUE)
+  M2_testing <- matrix(rep(1, (true_r*(true_p + 1))),
+                       ncol = (true_p + 1),
+                       nrow = true_r, byrow = TRUE)
+  M_testing <- rbind(M1_testing, M2_testing)
 
 
-  # Incorrect ImageName format
-  expect_error(mplnDataGenerator(nObservations = 50,
-    dimensionality = 6,
-    mixingProportions = c(0.79, 0.21),
-    mu = rbind(trueMu1, trueMu2),
-    sigma = rbind(trueSigma1, trueSigma2),
-    produceImage = "Yes",
-    ImageName = 1234))
+  testthat::expect_error(simulatedMVdata <- mvplnDataGenerator(
+                                                  nOccasions = true_r,
+                                                  nResponses = true_p,
+                                                  nUnits = true_n,
+                                                  mixingProportions = c(0.79, 0.21),
+                                                  matrixMean = M_testing,
+                                                  phi = true_Phi_all,
+                                                  omega = true_Omega_all))
+
+
+  # Generating simulated data - Phi has incorrect dimension
+  # Phi is a r x r matrix
+  library(clusterGeneration)
+  # Covariance matrix containing variances and covariances between r occasions
+  Phi1_testing <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
+                                                       dim = (true_r + 1),
+                                                       rangeVar = c(1, 1.7))$Sigma
+  Phi1_testing[1, 1] <- 1 # for identifiability issues
+  Phi2_testing <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
+                                                       dim = (true_r + 1),
+                                                       rangeVar = c(0.7, 0.7))$Sigma
+  Phi2_testing[1, 1] <- 1 # for identifiability issues
+  Phi_testing <- rbind(Phi1_testing, Phi2_testing)
+
+  testthat::expect_error(simulatedMVdata <- mvplnDataGenerator(
+                                                    nOccasions = true_r,
+                                                    nResponses = true_p,
+                                                    nUnits = true_n,
+                                                    mixingProportions = c(0.79, 0.21),
+                                                    matrixMean = true_M_all,
+                                                    phi = Phi_testing,
+                                                    omega = true_Omega_all))
+
+  # Generating simulated data - omega has incorrect dimension
+  # Omega is a p x p matrix
+  # Covariance matrix containing variance and covariances of p responses/variables
+  Omega1_testing <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
+                                                         dim = (true_p + 1),
+                                                         rangeVar = c(1, 1.7))$Sigma
+  Omega2_testing <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
+                                                         dim = (true_p + 1),
+                                                         rangeVar = c(0.7, 0.7))$Sigma
+  Omega_testing <- rbind(Omega1_testing, Omega2_testing)
+
+  testthat::expect_error(simulatedMVdata <- mvplnDataGenerator(
+                                                    nOccasions = true_r,
+                                                    nResponses = true_p,
+                                                    nUnits = true_n,
+                                                    mixingProportions = c(0.79, 0.21),
+                                                    matrixMean = true_M_all,
+                                                    phi = true_Phi_all,
+                                                    omega = Omega_testing))
+
+
+  # Generating simulated data - omega is not a matrix, but a data frame
+  # Omega is a p x p matrix
+  testthat::expect_error(simulatedMVdata <- mvplnDataGenerator(
+                                                    nOccasions = true_r,
+                                                    nResponses = true_p,
+                                                    nUnits = true_n,
+                                                    mixingProportions = c(0.79, 0.21),
+                                                    matrixMean = true_M_all,
+                                                    phi = true_Phi_all,
+                                                    omega = data.frame(true_Omega_all)))
 
 })
