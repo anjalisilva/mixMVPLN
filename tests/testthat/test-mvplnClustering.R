0d0ea312f683bd0a18b04a42d066a38723187ca0
Anjali Silva <anjali.silva@uhnresearch.ca>
2020-03-24 23:44:51 -0400

    Updated testing for clustering

--- tests/testthat/test-mvplnClustering.R
+++ tests/testthat/test-mvplnClustering.R
@@ -7,7 +7,7 @@ test_that("Checking clustering results", {
   true_G <- 2 # number of total G
   true_r <- 2 # number of total occasions
   true_p <- 3 # number of total responses
-  true_n <- 20 # number of total units
+  true_n <- 70 # number of total units
 
   # M is a r x p matrix
   true_M1 <- matrix(rep(6, (true_r * true_p)),
@@ -83,153 +83,222 @@ context("Checking for invalid user input")
 test_that("Data clustering error upon invalid user input", {
 
   # Generating simulated data
-  trueMu1 <- c(6.5, 6, 6, 6, 6, 6)
-  trueMu2 <- c(2, 2.5, 2, 2, 2, 2)
-
-  trueSigma1 <- diag(6) * 2
-  trueSigma2 <- diag(6)
-
   set.seed(1234)
-  simulatedCounts <- mplnDataGenerator(nObservations = 500,
-                                        dimensionality = 6,
+  true_G <- 2 # number of total G
+  true_r <- 2 # number of total occasions
+  true_p <- 3 # number of total responses
+  true_n <- 70 # number of total units
+
+  # M is a r x p matrix
+  true_M1 <- matrix(rep(6, (true_r * true_p)),
+                    ncol = true_p, nrow = true_r,
+                    byrow = TRUE)
+  true_M2 <- matrix(rep(1, (true_r * true_p)),
+                    ncol = true_p, nrow = true_r,
+                    byrow = TRUE)
+  true_M_all <- rbind(true_M1, true_M2)
+
+  # Phi is a r x r matrix
+  library(clusterGeneration)
+  # Covariance matrix containing variances and covariances between r occasions
+  true_Phi1 <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
+                                                    dim = true_r,
+                                                    rangeVar = c(1, 1.7))$Sigma
+  true_Phi1[1, 1] <- 1 # for identifiability issues
+
+  true_Phi2 <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
+                                                    dim = true_r,
+                                                    rangeVar = c(0.7, 0.7))$Sigma
+  true_Phi2[1, 1] <- 1 # for identifiability issues
+  true_Phi_all <- rbind(true_Phi1, true_Phi2)
+
+  # Omega is a p x p matrix
+  # Covariance matrix containing variance and covariances of p responses/variables
+  true_Omega1 <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
+                                                      dim = true_p,
+                                                      rangeVar = c(1, 1.7))$Sigma
+  true_Omega2 <- clusterGeneration::genPositiveDefMat(covMethod = "unifcorrmat",
+                                                      dim = true_p,
+                                                      rangeVar = c(0.7, 0.7))$Sigma
+  true_Omega_all <- rbind(true_Omega1, true_Omega2)
+
+  simulatedMVdata <- mvplnDataGenerator(nOccasions = true_r,
+                                        nResponses = true_p,
+                                        nUnits = true_n,
                                         mixingProportions = c(0.79, 0.21),
-                                        mu = rbind(trueMu1, trueMu2),
-                                        sigma = rbind(trueSigma1, trueSigma2),
-                                        produceImage = "No")
+                                        matrixMean = true_M_all,
+                                        phi = true_Phi_all,
+                                        omega = true_Omega_all)
+
+  # Clustering simulated matrix variate count data
+  clusteringResults <- mvplnClustering(dataset = simulatedMVdata$dataset,
+                                       membership = simulatedMVdata$truemembership,
+                                       gmin = 1,
+                                       gmax = 2,
+                                       nChains = 3,
+                                       nIterations = 300,
+                                       initMethod = "kmeans",
+                                       nInitIterations = 1,
+                                       normalize = "Yes",
+                                       numNodes = 2)
 
   # dataset provided as character
-  expect_error(mplnParallel(dataset = "dataset",
-                            membership = simulatedCounts$trueMembership,
-                            gmin = 1,
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = 1000,
-                            initMethod = "kmeans",
-                            nInitIterations = 3,
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                               dataset = "simulatedMVdata$dataset",
+                               membership = simulatedMVdata$truemembership,
+                               gmin = 1,
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = 300,
+                               initMethod = "kmeans",
+                               nInitIterations = 1,
+                               normalize = "Yes",
+                               numNodes = 2))
 
   # dataset provided as logical
-  expect_error(mplnParallel(dataset = NA,
-                            membership = simulatedCounts$trueMembership,
-                            gmin = 1,
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = 1000,
-                            initMethod = "kmeans",
-                            nInitIterations = 3,
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                              dataset = NA,
+                              membership = simulatedMVdata$truemembership,
+                              gmin = 1,
+                              gmax = 2,
+                              nChains = 3,
+                              nIterations = 300,
+                              initMethod = "kmeans",
+                              nInitIterations = 1,
+                              normalize = "Yes",
+                              numNodes = 2))
 
   # Incorrect size for true membership
-  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
-                            membership = simulatedCounts$trueMembership[-1],
-                            gmin = 1,
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = 1000,
-                            initMethod = "kmeans",
-                            nInitIterations = 3,
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                               dataset = simulatedMVdata$dataset,
+                               membership = simulatedMVdata$truemembership[-1],
+                               gmin = 1,
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = 300,
+                               initMethod = "kmeans",
+                               nInitIterations = 1,
+                               normalize = "Yes",
+                               numNodes = 2))
 
   # Incorrect class for true membership
-  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
-                            membership = "trueMembership",
-                            gmin = 1,
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = 1000,
-                            initMethod = "kmeans",
-                            nInitIterations = 3,
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                              dataset = simulatedMVdata$dataset,
+                              membership = "trueMembership",
+                              gmin = 1,
+                              gmax = 2,
+                              nChains = 3,
+                              nIterations = 300,
+                              initMethod = "kmeans",
+                              nInitIterations = 1,
+                              normalize = "Yes",
+                              numNodes = 2))
 
   # Incorrect input type for gmin
-  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
-                            membership = simulatedCounts$trueMembership,
-                            gmin = "1",
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = 1000,
-                            initMethod = "kmeans",
-                            nInitIterations = 3,
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                              dataset = simulatedMVdata$dataset,
+                              membership = simulatedMVdata$truemembership,
+                              gmin = "1",
+                              gmax = 2,
+                              nChains = 3,
+                              nIterations = 300,
+                              initMethod = "kmeans",
+                              nInitIterations = 1,
+                              normalize = "Yes",
+                              numNodes = 2))
 
   # Incorrect input for gmin and gmax
-  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
-                            membership = simulatedCounts$trueMembership,
-                            gmin = 5,
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = 1000,
-                            initMethod = "kmeans",
-                            nInitIterations = 3,
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                              dataset = simulatedMVdata$dataset,
+                              membership = simulatedMVdata$truemembership,
+                              gmin = 5,
+                              gmax = 2,
+                              nChains = 3,
+                              nIterations = 300,
+                              initMethod = "kmeans",
+                              nInitIterations = 1,
+                              normalize = "Yes",
+                              numNodes = 2))
 
 
   # Incorrect input type for nChains
-  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
-                            membership = simulatedCounts$trueMembership,
-                            gmin = 1,
-                            gmax = 2,
-                            nChains = "3",
-                            nIterations = 1000,
-                            initMethod = "kmeans",
-                            nInitIterations = 3,
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                              dataset = simulatedMVdata$dataset,
+                              membership = simulatedMVdata$truemembership,
+                              gmin = 1,
+                              gmax = 2,
+                              nChains = "3",
+                              nIterations = 300,
+                              initMethod = "kmeans",
+                              nInitIterations = 1,
+                              normalize = "Yes",
+                              numNodes = 2))
 
   # Incorrect input type for nIterations
-  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
-                            membership = simulatedCounts$trueMembership,
-                            gmin = 1,
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = "1000",
-                            initMethod = "kmeans",
-                            nInitIterations = 3,
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                              dataset = simulatedMVdata$dataset,
+                              membership = simulatedMVdata$truemembership,
+                              gmin = 1,
+                              gmax = 2,
+                              nChains = 3,
+                              nIterations = "300",
+                              initMethod = "kmeans",
+                              nInitIterations = 1,
+                              normalize = "Yes",
+                              numNodes = 2))
 
   # Incorrect input type for initMethod
-  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
-                            membership = simulatedCounts$trueMembership,
-                            gmin = 1,
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = 1000,
-                            initMethod = "other",
-                            nInitIterations = 3,
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                              dataset = simulatedMVdata$dataset,
+                              membership = simulatedMVdata$truemembership,
+                              gmin = 1,
+                              gmax = 2,
+                              nChains = 3,
+                              nIterations = 300,
+                              initMethod = "other",
+                              nInitIterations = 1,
+                              normalize = "Yes",
+                              numNodes = 2))
 
 
   # Incorrect input type for initMethod
-  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
-                            membership = simulatedCounts$trueMembership,
-                            gmin = 1,
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = 1000,
-                            initMethod = NA,
-                            nInitIterations = 3,
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                              dataset = simulatedMVdata$dataset,
+                              membership = simulatedMVdata$truemembership,
+                              gmin = 1,
+                              gmax = 2,
+                              nChains = 3,
+                              nIterations = 300,
+                              initMethod = NA,
+                              nInitIterations = 1,
+                              normalize = "Yes",
+                              numNodes = 2))
 
   # Incorrect input type for nInitIterations
-  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
-                            membership = simulatedCounts$trueMembership,
-                            gmin = 1,
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = 1000,
-                            initMethod = "kmeans",
-                            nInitIterations = "3",
-                            normalize = "Yes"))
+  testthat::expect_error(mvplnClustering(
+                              dataset = simulatedMVdata$dataset,
+                              membership = simulatedMVdata$truemembership,
+                              gmin = 1,
+                              gmax = 2,
+                              nChains = 3,
+                              nIterations = 300,
+                              initMethod = "kmeans",
+                              nInitIterations = "1",
+                              normalize = "Yes",
+                              numNodes = 2))
 
   # Incorrect input type for normalize
-  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
-                            membership = simulatedCounts$trueMembership,
-                            gmin = 1,
-                            gmax = 2,
-                            nChains = 3,
-                            nIterations = 1000,
-                            initMethod = "kmeans",
-                            nInitIterations = 3,
-                            normalize = "Other"))
+  testthat::expect_error(mvplnClustering(
+                              dataset = simulatedMVdata$dataset,
+                              membership = simulatedMVdata$truemembership,
+                              gmin = 1,
+                              gmax = 2,
+                              nChains = 3,
+                              nIterations = 300,
+                              initMethod = "kmeans",
+                              nInitIterations = 1,
+                              normalize = "other",
+                              numNodes = 2))
 
 })
 
